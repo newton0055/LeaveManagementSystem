ec6c869b78a33d5f5571232e3c7bacd70e1ce489
Chukwudi Chidubem <chidubemchukwudi@gmail.com>
2025-01-03 05:05:42 +0100

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC Mastery",
+                DateOfBirth = new DateTime(2003,1,29)
+            };
+            return View(data);
+        }
+    }
+}
